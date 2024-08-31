c1eabe53324ec2eb6b090c9675078572279d8308
xihale <xihale.top@qq.com>
2024-08-31 17:04:07 +0800

    fix: vite.config

--- adapters/vercel-edge/vite.config.ts
+++ adapters/vercel-edge/vite.config.ts
@@ -1,6 +1,6 @@
 import { vercelEdgeAdapter } from "@builder.io/qwik-city/adapters/vercel-edge/vite";
 import { extendConfig } from "@builder.io/qwik-city/vite";
-import baseConfig from "../../vite.config";
+import baseConfig from "../../vite.config.mjs";
 
 export default extendConfig(baseConfig, () => {
   return {
