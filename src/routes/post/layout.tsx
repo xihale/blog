9dd47d16b032cc56bcec7d3abeae771d62642a4f
xihale <xihale.top@qq.com>
2024-12-07 21:10:23 +0800

    meta

--- src/routes/post/layout.tsx
+++ src/routes/post/layout.tsx
@@ -7,9 +7,10 @@ export default component$(() => {
   const { title, frontmatter } = useDocumentHead();
 
   return (
-    <>
+    <div>
       <h1 class={styles.title}>{title}</h1>
       <div class={styles.post_meta}>
+        <div class={styles.license}>license: CC BY 4.0</div>
         <div class={styles.date}>
           {frontmatter.date ? (
             <span class={styles.post_date}>Posted {frontmatter.date}</span>
@@ -22,8 +23,31 @@ export default component$(() => {
             <></>
           )}
         </div>
+        {frontmatter.category && (
+          <div class={styles.category}>
+            category:{" "}
+            {frontmatter.category.map((item: string, idx: number) => (
+              <span class={styles.category_item} key={idx}>
+                {item}
+              </span>
+            ))}
+          </div>
+        )}
+        {frontmatter.tags && frontmatter.category && " | "}
+        {frontmatter.tags && (
+          <div class={styles.tags}>
+            tags:{" "}
+            {frontmatter.tags.map((item: string, idx: number) => (
+              <span class={styles.tags_item} key={idx}>
+                {item}
+              </span>
+            ))}
+          </div>
+        )}
+        <div>
+          <Slot />
+        </div>
       </div>
-      <Slot />
-    </>
+    </div>
   );
 });
