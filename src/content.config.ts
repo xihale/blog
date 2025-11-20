ccf8b195617031d3e14b3c3b3f9f192b8243c95a
xihale <xihale.top@qq.com>
2025-11-20 13:26:15 +0800

    fix: type & check

--- src/content.config.ts
+++ src/content.config.ts
@@ -13,6 +13,8 @@ const blog = defineCollection({
 			pubDate: z.coerce.date(),
 			updatedDate: z.coerce.date().optional(),
 			heroImage: image().optional(),
+			draft: z.boolean().optional(),
+			tags: z.array(z.string()).optional(),
 		}),
 });
 
